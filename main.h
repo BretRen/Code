0a7300f89c33f71b1089d00860d4b7dedd7085a7
BretRen <135674689+BretRen@users.noreply.github.com>
2024-02-21 21:46:10 -0800

    Add files via upload

---
+++ main.h
@@ -0,0 +1,21 @@
+#define sb cout
+
+
+#define a1 ()
+#define a2 (
+#define a3 )
+#define b1 "Hello World" 
+#define b2 main
+#define b3 "pause"
+#define c1 include
+#define c2 using
+#define c3 namespace
+#define c4 std
+#define d1 ;
+#define d2 0
+#define cnm <<
+#define d3 endl
+#define e1 system
+#define e2 return
+#define e3 {
+#define e4 }
